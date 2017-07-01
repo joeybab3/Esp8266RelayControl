35a432af62460f7febae477a77d2b1357f4f9f15
Joey Babcock <joey_babcock@yahoo.com>
2017-07-01 16:49:15 -0700

    too much change

--- RelayShieldControlOTA/RelayShieldControlOTA.ino/RelayShieldControlOTA.ino.ino
+++ RelayShieldControlOTA/RelayShieldControlOTA.ino/RelayShieldControlOTA.ino.ino
@@ -1,5 +1,7 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
@@ -12,6 +14,7 @@
 #define HTTPPORT 4567
 
 
+MDNSResponder mdns;
 WiFiClient client;
 WiFiManager wifiManager;
 ESP8266WebServer server(HTTPPORT);
@@ -36,6 +39,35 @@ void setup()
   wifiManager.autoConnect();
 
   Serial.println("connected to WiFi");
+  if (mdns.begin("garage", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+
+  ArduinoOTA.setHostname("garage");
+  ArduinoOTA.setPort(8266);
+  
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   /* WebServer stuff */
 
@@ -59,6 +91,7 @@ void ethConnectError()
 
 void loopStuff()
 {
+  ArduinoOTA.handle();
   server.handleClient();
 }
 
